b09543c877304e2811ec2907bf269495f413f5e5
harrydrippin <hj332921@gmail.com>
2019-01-05 19:13:21 +0900

    [6-32] Dynamic Block Difficulty
    Signed-off-by: harrydrippin <hj332921@gmail.com>

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,6 +1,6 @@
 // SHA-256 함수를 불러옵니다.
 const SHA256 = require('crypto-js/sha256');
-const { DIFFICULTY } = require("../config");
+const { DIFFICULTY, MINE_RATE } = require("../config");
 
 /**
  * 블록체인에 달릴 실제 블록에 대한 클래스입니다.
@@ -13,13 +13,16 @@ class Block {
    * @param {string} hash 지금 만드는 블록의 Hash
    * @param {*} data 지금 만드는 블록에 들어갈 Data
    * @param {string} nonce Nonce 값
+   * @param {number} difficulty 채굴 난이도
    */
-  constructor(timestamp, lastHash, hash, data, nonce) {
+  constructor(timestamp, lastHash, hash, data, nonce, difficulty) {
     this.timestamp = timestamp;
     this.lastHash = lastHash;
     this.hash = hash;
     this.data = data;
     this.nonce = nonce;
+    // 지정된 Difficulty가 없을 경우 Default 값을 사용합니다.
+    this.difficulty = difficulty || DIFFICULTY;
   }
 
   /**
@@ -28,11 +31,12 @@ class Block {
   toString() {
     // Hash는 길기 때문에 앞부분만 표시합니다.
     return `Block -
-      Timestamp : ${this.timestamp}
-      Last Hash : ${this.lastHash.substring(0, 10)}
-      Hash      : ${this.hash.substring(0, 10)}
-      Nonce     : ${this.nonce}
-      Data      : ${this.data}
+      Timestamp  : ${this.timestamp}
+      Last Hash  : ${this.lastHash.substring(0, 10)}
+      Hash       : ${this.hash.substring(0, 10)}
+      Nonce      : ${this.nonce}
+      Difficulty : ${this.difficulty}
+      Data       : ${this.data}
     `;
   }
 
@@ -50,7 +54,9 @@ class Block {
       // Genesis Block의 데이터로써 빈 Array를 넣습니다.
       [],
       // 최초의 Nonce 값입니다.
-      0
+      0,
+      // 최초의 채굴 난이도 값입니다.
+      DIFFICULTY
     );
   }
 
@@ -64,6 +70,8 @@ class Block {
     let hash, timestamp;
     // 이전 Block의 Hash를 꺼내옵니다.
     const lastHash = lastBlock.hash;
+    // 이전 Block에서 채굴 난이도 값을 가져옵니다.
+    let { difficulty } = lastBlock;
     // Nonce 값을 보관할 변수를 선언합니다.
     let nonce = 0;
     
@@ -73,12 +81,14 @@ class Block {
       nonce++;
       // 현재의 시간 값을 받아옵니다.
       timestamp = Date.now();
+      // 시간이 소요되고 있으므로 매 반복마다 Difficulty를 다시 계산합니다.
+      difficulty = Block.adjustDifficulty(lastBlock, timestamp);
       // Block.hash 함수를 사용하여 현재 Nonce 기준의 Hash를 만듭니다.
-      hash = Block.hash(timestamp, lastHash, data, nonce);
-    } while (hash.substring(0, DIFFICULTY) !== "0".repeat(DIFFICULTY));
+      hash = Block.hash(timestamp, lastHash, data, nonce, difficulty);
+    } while (hash.substring(0, difficulty) !== "0".repeat(difficulty));
 
     // 새로 만들어진 Block을 반환합니다.
-    return new this(timestamp, lastHash, hash, data, nonce);
+    return new this(timestamp, lastHash, hash, data, nonce, difficulty);
   }
 
   /**
@@ -87,10 +97,11 @@ class Block {
    * @param {string} lastHash lastHash 값
    * @param {*} data data 값
    * @param {string} nonce nonce 값
+   * @param {number} difficulty difficulty 값
    */
-  static hash(timestamp, lastHash, data, nonce) {
+  static hash(timestamp, lastHash, data, nonce, difficulty) {
     // CryptoJS의 SHA256 함수를 이용하여 Hash를 만들어서 String으로 반환합니다.
-    return SHA256(`${timestamp}${lastHash}${data}${nonce}`).toString();
+    return SHA256(`${timestamp}${lastHash}${data}${nonce}${difficulty}`).toString();
   }
 
   /**
@@ -98,8 +109,22 @@ class Block {
    * @param {Block} block 
    */
   static blockHash(block) {
-    const { timestamp, lastHash, data, nonce } = block;
-    return Block.hash(timestamp, lastHash, data, nonce);
+    const { timestamp, lastHash, data, nonce, difficulty } = block;
+    return Block.hash(timestamp, lastHash, data, nonce, difficulty);
+  }
+
+  /**
+   * 채굴 난이도를 시간 값을 기준으로 조절합니다.
+   * @param {Block} lastBlock 이전 Block
+   * @param {Date} currentTime 채굴 시점의 시각
+   */
+  static adjustDifficulty(lastBlock, currentTime) {
+    // 이전 Block의 채굴 난이도 값을 가져옵니다.
+    let { difficulty } = lastBlock;
+    // 이전 Block이 생성된 시점으로부터 MINE_RATE초 지난 시점 이전이면 Difficulty를 1 증가시킵니다.
+    // 해당 시점 이후이면 너무 오랫동안 Block이 나오지 않고 있으므로 Difficulty를 1 감소시킵니다.
+    difficulty = lastBlock.timestamp + MINE_RATE > currentTime ? difficulty + 1 : difficulty - 1;
+    return difficulty;
   }
 }
 
--- config.js
+++ config.js
@@ -1,6 +1,10 @@
 // PoW 시스템에 쓰일 Difficulty 상수를 선언합니다.
 const DIFFICULTY = 3;
 
+// PoW 시스템에 쓰일 Mining Rate 값을 선언합니다.
+// 3초 정도의 시간마다 하나의 Block이 채굴되도록 설정합니다.
+const MINE_RATE = 3000;
+
 module.exports = {
-  DIFFICULTY
+  DIFFICULTY, MINE_RATE
 };
