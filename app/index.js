c7896a3715479d9ca8ded90018b9a167cc84d4a1
harrydrippin <hj332921@gmail.com>
2019-01-05 17:41:48 +0900

    [4-23] Mine Blocks Post Request
    Signed-off-by: harrydrippin <hj332921@gmail.com>

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const Blockchain = require("../blockchain");
 
 // 이 서버가 어느 포트에 열릴지를 결정합니다.
@@ -9,11 +10,26 @@ const HTTP_PORT = process.env.HTTP_PORT || 3001;
 const app = express();
 const bc = new Blockchain();
 
+// POST 요청에서 JSON을 Body로 받을 수 있도록 Middleware를 설정합니다.
+app.use(bodyParser.json());
+
 /**
- * Blockchain이 가진 Block들을 반환합니다.
+ * Blockchain이 가진 Block들을 반환하는 API입니다.
  */
 app.get("/blocks", (req, res) => {
   res.json(bc.chain);
+});
+
+/**
+ * Block을 Chain에 더하는 API입니다.
+ */
+app.post("/mine", (req, res) => {
+  // POST 요청에 같이 올라온 Block을 Chain에 더합니다.
+  const block = bc.addBlock(req.body.data);
+  console.log(`New block added: ${block.toString()}`);
+
+  // GET blocks로 리디렉션합니다.
+  res.redirect("/blocks");
 });
 
 // 주어진 포트 번호에 서버를 엽니다.
