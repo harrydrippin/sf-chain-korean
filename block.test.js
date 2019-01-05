1972f52a086f7ea4a080ef341a103454259f24f9
harrydrippin <hj332921@gmail.com>
2019-01-05 16:33:38 +0900

    [2-13] Test the Block
    Signed-off-by: harrydrippin <hj332921@gmail.com>

---
+++ block.test.js
@@ -0,0 +1,33 @@
+const Block = require("./block");
+
+/**
+ * Block 클래스에 대한 테스트를 정의합니다.
+ * Jest를 사용합니다.
+ */
+describe('Block', () => {
+  let data, lastBlock, block;
+
+  /**
+   * 테스트를 하기 전에 미리 호출되는 함수입니다.
+   * 적절한 Data와 lastBlock을 만들고, 이를 통해 Block을 하나 생성해둡니다.
+   */
+  beforeEach(() => {
+    data = 'bar';
+    lastBlock = Block.genesis();
+    block = Block.mineBlock(lastBlock, data);
+  });
+
+  /**
+   * 생성된 Block의 Data가 실제 Data와 일치하는가?
+   */
+  it("sets the `data` to match the input", () => {
+    expect(block.data).toEqual(data);
+  });
+
+  /**
+   * 생성된 Block의 lastHash가 실제로 마지막 Block의 Hash와 일치하는가?
+   */
+  it("sets the `lastHash` to match the hash of the last block", () => {
+    expect(block.lastHash).toEqual(lastBlock.hash);
+  });
+});
