ba01451a9d151922f2e4296feaacb6e983efd6f0
harrydrippin <hj332921@gmail.com>
2019-01-06 14:46:59 +0900

    [7-38] Create a Transaction
    Signed-off-by: harrydrippin <hj332921@gmail.com>

--- chain-util.js
+++ chain-util.js
@@ -1,13 +1,22 @@
 const EC = require("elliptic").ec;
-// Bitcoin에서 쓰는 Elliptic Curve를 불러옵니다.
+const uuidV1 = require("uuid/v1");
+
+// Bitcoin에서 쓰는 Elliptic Curve인 SECP256K1을 불러옵니다.
 const ec = new EC("secp256k1");
 
 class ChainUtil {
   /**
-   * elliptic 모듈을 이용해서 KeyPair를 생성해냅니다.
+   * elliptic 모듈을 이용해서 KeyPair를 생성합니다.
    */
   static genKeyPair() {
     return ec.genKeyPair();
+  }
+
+  /**
+   * UUID를 생성합니다.
+   */
+  static id() {
+    return uuidV1();
   }
 }
 
---
+++ wallet/transaction.js
@@ -0,0 +1,47 @@
+const ChainUtil = require("../chain-util");
+
+/**
+ * Transaction 클래스입니다.
+ */
+class Transaction {
+  /**
+   * Transaction 클래스의 생성자입니다.
+   */
+  constructor() {
+    // UUID를 통해 Unique한 ID를 부여합니다.
+    this.id = ChainUtil.id();
+    // 거래의 Input을 넣습니다.
+    this.input = null;
+    // 거래의 Output이 담길 Array를 넣습니다.
+    this.outputs = [];
+  }
+
+  /**
+   * 새로운 Transaction을 생성합니다.
+   * @param {Wallet} senderWallet 보내는 사람의 지갑
+   * @param {string} recipient 받는 사람의 주소
+   * @param {number} amount 보내는 양
+   */
+  static newTransaction(senderWallet, recipient, amount) {
+    // 새로운 Transaction 객체를 만듭니다.
+    const transaction = new this();
+    
+    // 지금 가진 잔고보다 더 많은 돈을 보내는 것은 아닌지 확인합니다.
+    if (amount > senderWallet.balance) {
+      console.log(`Amount: ${amount} exceeds balance.`);
+      return;
+    }
+
+    // Transaction의 Output을 만듭니다.
+    transaction.outputs.push(...[
+      // 송신자의 Balance를 갱신하는 Output입니다.
+      { amount: senderWallet.balance - amount, address: senderWallet.publicKey },
+      // 수신자에게 돈을 보내는 것을 확인하는 Outputㅇ비니다.
+      { amount, address: recipient }
+    ]);
+
+    return transaction;
+  }
+}
+
+module.exports = Transaction;
